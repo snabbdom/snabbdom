2f878813b18663d378bcec20c474b8fe6c36e35b
Sylvain Prat <sylvain.prat@net-ng.com>
2016-01-11 09:33:46 +0100

    Added a bunch of failing tests for thunks

--- test/thunk.js
+++ test/thunk.js
@@ -63,4 +63,78 @@ describe('thunk', function() {
     assert.equal(elm.firstChild.innerHTML, 'Number is 2');
     assert.equal(called, 2);
   });
+  it('renders correctly when root', function() {
+      var called = 0;
+      function numberInSpan(n) {
+        called++;
+        return h('span', 'Number is ' + n);
+      }
+      var vnode1 = thunk('num', numberInSpan, 1);
+      var vnode2 = thunk('num', numberInSpan, 1);
+      var vnode3 = thunk('num', numberInSpan, 2);
+
+      patch(vnode0, vnode1);
+      assert.equal(elm.innerHTML, 'Number is 1');
+
+      patch(vnode1, vnode2);
+      assert.equal(elm.innerHTML, 'Number is 1');
+
+      patch(vnode2, vnode3);
+      assert.equal(elm.innerHTML, 'Number is 2');
+      assert.equal(called, 2);
+  });
+  it('can mutate its root tag', function() {
+    function oddEven(n) {
+      var oddEvenSel = (n % 2) ? 'div.odd' : 'p.even';
+      return h(oddEvenSel, n);
+    }
+    var vnode1 = h('div', [thunk('oddEven', oddEven, 1)]);
+    var vnode2 = h('div', [thunk('oddEven', oddEven, 4)]);
+
+    patch(vnode0, vnode1);
+    assert.equal(elm.firstChild.tagName, 'DIV');
+    assert.equal(elm.firstChild.className, 'odd');
+
+    patch(vnode1, vnode2);
+    assert.equal(elm.firstChild.tagName, 'P');
+    assert.equal(elm.firstChild.className, 'even');
+  });
+  it('can be replaced and removed', function() {
+      function numberInSpan(n) {
+        return h('span.numberInSpan', 'Number is ' + n);
+      }
+      function oddEven(n) {
+        var oddEvenClass = (n % 2) ? '.odd' : '.even';
+        return h('div' + oddEvenClass, 'Number is ' + n);
+      }
+      var vnode1 = h('div', [thunk('num', numberInSpan, 1)]);
+      var vnode2 = h('div', [thunk('oddEven', oddEven, 4)]);
+
+      patch(vnode0, vnode1);
+      assert.equal(elm.firstChild.tagName, 'SPAN');
+      assert.equal(elm.firstChild.className, 'numberInSpan');
+
+      patch(vnode1, vnode2);
+      assert.equal(elm.firstChild.tagName, 'DIV');
+      assert.equal(elm.firstChild.className, 'even');
+  });
+  it('can be replaced and removed when root', function() {
+      function numberInSpan(n) {
+          return h('span.numberInSpan', 'Number is ' + n);
+        }
+        function oddEven(n) {
+          var oddEvenClass = (n % 2) ? '.odd' : '.even';
+          return h('div' + oddEvenClass, 'Number is ' + n);
+        }
+        var vnode1 = thunk('num', numberInSpan, 1);
+        var vnode2 = thunk('oddEven', oddEven, 4);
+
+        patch(vnode0, vnode1);
+        assert.equal(elm.firstChild.tagName, 'SPAN');
+        assert.equal(elm.firstChild.className, 'numberInSpan');
+
+        patch(vnode1, vnode2);
+        assert.equal(elm.firstChild.tagName, 'DIV');
+        assert.equal(elm.firstChild.className, 'even');
+  });
 });
