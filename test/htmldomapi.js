166b8eca23bc6d97005d460805aec5e54fbc8402
Aron Allen <aro@danskebank.com>
2017-01-02 09:42:14 +0100

    added test

---
+++ test/htmldomapi.js
@@ -0,0 +1,24 @@
+var assert = require('assert');
+
+var snabbdom = require('../snabbdom');
+var h = require('../h').default;
+var patch = snabbdom.init([]);
+
+describe('svg', function () {
+ var elm, vnode0;
+ beforeEach(function() {
+   elm = document.createElement('svg');
+   vnode0 = elm;
+ });
+ it('removes svg elements', function(){
+   var a = h('svg', {}, [
+    h('g'),
+    h('g')
+   ]);
+   var b = h('svg', {}, [
+    h('g')
+   ]);
+   var result = patch(patch(vnode0, a), b).elm;
+   assert.equal(result.childNodes.length, 1); 
+ });
+})
