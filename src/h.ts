a8cc8913a719c328d5841b162b509d6f56470631
dmitriz <dmitri14@gmail.com>
2017-04-23 15:09:21 +0100

    Move length evaluation out of the loop

--- src/h.ts
+++ src/h.ts
@@ -33,7 +33,7 @@ export function h(sel: any, b?: any, c?: any): VNode {
     else { data = b; }
   }
   if (is.array(children)) {
-    for (i = 0; i < children.length; ++i) {
+    for (var i = 0, l = children.length; i < l; ++i) {
       if (is.primitive(children[i])) children[i] = (vnode as any)(undefined, undefined, undefined, children[i]);
     }
   }
