f7aa66d8653498c06b0031abd7c9f5ed9517aa8f
aymeric <atrotignon@ekinops.net>
2017-05-05 09:43:21 +0200

    PR for issue #262 (remove draggable for boolean attributes)

--- src/modules/attributes.ts
+++ src/modules/attributes.ts
@@ -2,7 +2,7 @@ import {VNode, VNodeData} from '../vnode';
 import {Module} from './module';
 
 const booleanAttrs = ["allowfullscreen", "async", "autofocus", "autoplay", "checked", "compact", "controls", "declare",
-                "default", "defaultchecked", "defaultmuted", "defaultselected", "defer", "disabled", "draggable",
+                "default", "defaultchecked", "defaultmuted", "defaultselected", "defer", "disabled",
                 "enabled", "formnovalidate", "hidden", "indeterminate", "inert", "ismap", "itemscope", "loop", "multiple",
                 "muted", "nohref", "noresize", "noshade", "novalidate", "nowrap", "open", "pauseonexit", "readonly",
                 "required", "reversed", "scoped", "seamless", "selected", "sortable", "spellcheck", "translate",
