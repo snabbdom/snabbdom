546fe35ccf41c154b2d0bf5e59768e85d834e36c
Jeremy Apthorp <nornagon@nornagon.net>
2017-06-15 20:29:39 -0700

    Delete props only when they become undefined
    This allows setting a prop to `0` or `false`.

--- src/modules/props.ts
+++ src/modules/props.ts
@@ -14,7 +14,7 @@ function updateProps(oldVnode: VNode, vnode: VNode): void {
   props = props || {};
 
   for (key in oldProps) {
-    if (!props[key]) {
+    if (typeof props[key] === 'undefined') {
       delete (elm as any)[key];
     }
   }
