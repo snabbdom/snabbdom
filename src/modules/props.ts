774d51b682c215d2180150b4e83740c362f0e2b8
Jeremy Apthorp <nornagon@nornagon.net>
2017-07-09 17:48:31 -0700

    Use == null instead of typeof == 'undefined'

--- src/modules/props.ts
+++ src/modules/props.ts
@@ -14,7 +14,7 @@ function updateProps(oldVnode: VNode, vnode: VNode): void {
   props = props || {};
 
   for (key in oldProps) {
-    if (typeof props[key] === 'undefined') {
+    if (props[key] == null) {
       delete (elm as any)[key];
     }
   }
