381fc0d85eeb50601d90e9519d6773de1b553e8d
Shahar Or (mightyiam) <mightyiampresence@gmail.com>
2020-01-07 22:05:23 +0700

    fix: include JSX related module in package
    Fixes #503.

--- src/jsx-global.ts
+++ src/jsx-global.ts
@@ -5,6 +5,7 @@ declare global {
    * opt-in jsx intrinsic global interfaces
    * see: https://www.typescriptlang.org/docs/handbook/jsx.html#type-checking
    */
+  // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace JSX {
     type Element = VNode;
     interface IntrinsicElements {
