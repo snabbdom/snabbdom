896dd21624c8f03d0ae16a96a3aef55fed674352
Shahar Or (mightyiam) <mightyiampresence@gmail.com>
2020-01-08 22:28:03 +0700

    test: run jsx test
    Fixes #532.

--- karma.conf.js
+++ karma.conf.js
@@ -19,7 +19,7 @@ module.exports = function (config) {
     basePath: '.',
     frameworks: ['mocha', 'karma-typescript'],
     // list of files / patterns to load in the browser
-    files: [{ pattern: 'src/**/*.ts' }],
+    files: [{ pattern: 'src/**/*' }],
     plugins: [
       'karma-mocha',
       'karma-chrome-launcher',
