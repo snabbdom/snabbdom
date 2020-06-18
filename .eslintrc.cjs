41cb3596e8898399545f02ff8205a1d45f62f391
Shahar Or (mightyiam) <mightyiampresence@gmail.com>
2020-06-18 16:42:06 +0700

    dx(docs): lint code examples

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,6 +6,38 @@ module.exports = {
       './src/test/tsconfig.json',
     ]
   },
+  plugins: [
+    'markdown',
+  ],
+  overrides: [
+    {
+      files: ['**/*.md'],
+      processor: 'markdown/markdown'
+    },
+    {
+      files: ['**/*.md/*.@(mjs|ts)'],
+      rules: {
+        'array-bracket-spacing': 'off',
+        'import/first': 'off',
+        'import/newline-after-import': 'off',
+        'key-spacing': 'off',
+        'max-statements-per-line': 'off',
+        'no-multi-spaces': 'off',
+        'no-undef': 'off',
+        'no-unused-expressions': 'off',
+        'no-unused-vars': 'off',
+        'object-curly-newline': 'off',
+        'object-curly-spacing': 'off',
+        'object-property-newline': 'off',
+        'quote-props': 'off',
+        'space-before-blocks': 'off',
+        'space-before-function-paren': 'off',
+        indent: 'off',
+        quotes: 'off',
+        semi: 'off',
+      },
+    }
+  ],
   rules: {
     'import/newline-after-import': 'error',
     'max-statements-per-line': 'error',
