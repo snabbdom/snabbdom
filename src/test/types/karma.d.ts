489fc8bce3cfee6cd0b79770d512481ce9c2c14e
patomation <patomation@gmail.com>
2020-10-05 12:00:39 +0700

    feat(karma): move global __karma__ type into d.ts file
    Fixes #846

---
+++ src/test/types/karma.d.ts
@@ -0,0 +1,9 @@
+export {}
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+declare global {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  const __karma__: {
+    info(info: unknown): void
+  }
+}
