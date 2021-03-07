3e2661beb644b74e1c011ba4af56583f6771a49d
Isaac Shapira <fresheyeball@protonmail.com>
2021-03-06 17:46:45 -0700

    We should check if the key is "checked" as well as "value" to see if the dom got out of sync.

--- src/package/modules/props.ts
+++ src/package/modules/props.ts
@@ -19,7 +19,7 @@ function updateProps (oldVnode: VNode, vnode: VNode): void {
   for (key in props) {
     cur = props[key]
     old = oldProps[key]
-    if (old !== cur && (key !== 'value' || (elm as any)[key] !== cur)) {
+    if (old !== cur && ((key !== 'checked' && key !== 'value') || (elm as any)[key] !== cur)) {
       (elm as any)[key] = cur
     }
   }
