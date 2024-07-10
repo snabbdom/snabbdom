eda75b7378401b14ad0f8f15d8440700c7264093
Ame_x <121654029+EdamAme-x@users.noreply.github.com>
2024-07-10 14:06:53 +0900

    refactor: use PropertyKey instaed of key list

--- src/vnode.ts
+++ src/vnode.ts
@@ -7,7 +7,7 @@ import { Classes } from "./modules/class";
 import { Props } from "./modules/props";
 import { Dataset } from "./modules/dataset";
 
-export type Key = string | number | symbol;
+export type Key = PropertyKey;
 
 export interface VNode {
   sel: string | undefined;
