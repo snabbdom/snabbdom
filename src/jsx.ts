c0ea0e077dc3135ce9863c32dd3509783d63bb73
Simon Friis Vindum <simonfv@gmail.com>
2026-06-12 09:20:26 +0200

    Simplify JSX types

--- src/jsx.ts
+++ src/jsx.ts
@@ -103,37 +103,15 @@ export function jsx(
 namespace JSXTypes {
   export type Element = VNode;
 
-  /*
-  IfEquals/WritableKeys: https://stackoverflow.com/questions/52443276/how-to-exclude-getter-only-properties-from-type-in-typescript/52473108#52473108
-  */
-  export type IfEquals<X, Y, Output> =
-    (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y ? 1 : 2
-      ? Output
-      : never;
-
-  export type WritableKeys<T> = {
-    [P in keyof T]-?: IfEquals<
-      { [Q in P]: T[P] },
-      { -readonly [Q in P]: T[P] },
-      P
-    >;
-  }[keyof T];
-
   export type ElementProperties<T> = {
-    [Property in WritableKeys<T> as T[Property] extends
-      | string
-      | number
-      | null
-      | undefined
-      ? Property
-      : never]?: T[Property];
+    [P in keyof T as T[P] extends string | number | null | undefined
+      ? P
+      : never]?: T[P];
   };
-
-  export type VNodeProps<T> = ElementProperties<T> & Props;
 
   export type HtmlElements = {
     [Property in keyof HTMLElementTagNameMap]: VNodeData<
-      VNodeProps<HTMLElementTagNameMap[Property]>
+      ElementProperties<HTMLElementTagNameMap[Property]> & Props
     >;
   };
 
